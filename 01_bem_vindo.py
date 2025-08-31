e740b3ccf8f9adf98ade46c885ee3b44ff7f81c5
Denilson <61381742+denilsonssilva@users.noreply.github.com>
2025-08-31 11:13:47 -0300

    Add files via upload

---
+++ 01_bem_vindo.py
@@ -0,0 +1,6 @@
+nome = input('Digite o seu nome: ')
+
+print('Seja bem vindo', nome)
+
+print('Seja bem vindo {}'.format(nome))
+
---
+++ 03_soma.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+total = n1 + n2
+
+print('A soma dos valores digitados é:', total)
