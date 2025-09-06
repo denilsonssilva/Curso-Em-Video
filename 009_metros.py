8c857af358e376c35f6fb76d3b26ddf3559da551
Denilson <61381742+denilsonssilva@users.noreply.github.com>
2025-09-06 16:20:24 -0300

    Delete 009_metros.py

--- 009_metros.py
+++
@@ -1,7 +0,0 @@
-metros = float(input('Quantos metros tem da sua casa até o serviço: '))
-c = metros * 100
-m = metros * 1000
-
-print(f'{metros:.2f} metros é o mesmo que {c:.2f} centimetros da sua casa até o serviço.')
-print(f'{metros:.2f} metros é o mesmo que {m:.2f} milimetros.')
-
