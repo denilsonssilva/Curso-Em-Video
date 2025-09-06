7ca72163959a4a0a3fcd8b5993165c3898524124
Denilson <61381742+denilsonssilva@users.noreply.github.com>
2025-09-06 16:22:03 -0300

    Delete 021_cidade.py

--- 021_cidade.py
+++
@@ -1,7 +0,0 @@
-cid = input('Dgite o nome da sua Cidade: ').strip().lower()
-cid = cid.split()
-
-if cid[0] == 'santo':
-    print(f'O nome da sua cidade contém a palavra SANTO.')
-else:
-    print(f'O nome da sua cidade não tem SANTO.')
