7b59c802af55bebc9c720704143956de5afe3ebe
Denilson <61381742+denilsonssilva@users.noreply.github.com>
2025-09-06 12:57:07 -0300

    Update TELA-LOGIN.py

--- TELA-LOGIN.py
+++ TELA-LOGIN.py
@@ -1,24 +1,24 @@
 login = ['DENILSON','JORGE','MATHEUS']
 senha = ['123','569','321']
 
-#qtd e itens na lista
-com = len(login)
-
 tot = 0
-
+#Pergunta se o usuario tem login
 logon = input('Você possui login? [S/N]').upper().strip()
 
 while logon == 'S':
-
+    
     loginU = input('Digite o seu login: ').upper().strip()#usuario
     senhaU = input('Digite sua senha: ').strip()#senha usuario
 
-    for c in range (0, com):
-        if login[c] == loginU and senha[c] == senhaU:
-            print(f'Bem vindo ao Sistema {loginU}!')
-            logon = login[c]
-            tot += 1
-    else:        
+    for nome in login: #percorre toda a lista 'login' e adiciona cada posição a variavel nome
+        for sen in senha: #percorre toda a lista 'senha' e adiciona cada posição a variavel sen
+            if nome == loginU and sen == senhaU:
+                logon = nome
+                tot += 1
+    
+    if tot > 0:
+        print(f'Bem vindo ao Sistema {nome}!')
+    else:
         print('Você digitou a senha/usuario errado, tente novamente!')
         logon = input('Deseja continuar? [S/N]').upper().strip()
     
