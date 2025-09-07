65f20de9f14a376181702eb5fbdeeef755b376cb
Denilson <61381742+denilsonssilva@users.noreply.github.com>
2025-09-07 13:14:26 -0300

    Update 025_adivinhe-o-numero.py

--- Modulo-01/025_adivinhe-o-numero.py
+++ Modulo-01/025_adivinhe-o-numero.py
@@ -1,4 +1,5 @@
 import random
+from time import sleep as sl
 
 n1 = int(input('Tente adivinhar qual numero estou pensando de 0 a 5: '))
 
@@ -6,8 +7,14 @@ pc = random.randint(0, 5)
 
 while n1 != '':
     if n1 == pc:
+        print('PROCESSANDO...')
+        sl(2)
         print(f'\033[32mParabéns, o numero que eu estava pensando era o \033[32m{pc}.\033[m')
+        n1 = int(input('Jogue novamente, qual numero estou pensando de 0 a 5? '))
+        pc = random.randint(0, 5)
     else:
+        print('PROCESSANDO...')
+        sl(2)
         print(f'\033[31mO numero que eu pensei foi o \033[31m{pc}.\033[m')
         n1 = int(input('Tente novamente, qual numero estou pensando de 0 a 5? '))
         pc = random.randint(0, 5)
